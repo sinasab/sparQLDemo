cecd1bc101828dfb2fa960f61bfc7231dd4a2453
Sina Sabet <sinasabet81@gmail.com>
2016-11-03 16:35:16 -0400

    added dropdown for selecting prewritten queries

--- docs/compile.js
+++ docs/compile.js
@@ -1,2 +1,11 @@
 const pug = require('pug');
-console.log(pug.renderFile('layout.pug', {}));
+
+const prewrittenQueries = {
+  'clear': '',
+  'Senators\' Spouses': `SELECT ?res WHERE  {
+  ?src ?rel <http://dbpedia.org/class/yago/Senator110578471> .
+  ?src <http://dbpedia.org/property/spouse> ?res .
+}`,
+};
+
+console.log(pug.renderFile('layout.pug', { prewrittenQueries }));
