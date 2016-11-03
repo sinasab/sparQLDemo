7ff3b7b7b64a2424a93d2baf0e373a0364826120
Sina Sabet <sinasabet81@gmail.com>
2016-11-03 17:21:47 -0400

    shortening names of prewritten queries

--- docs/compile.js
+++ docs/compile.js
@@ -2,11 +2,11 @@ const pug = require('pug');
 
 const prewrittenQueries = {
   'clear': '',
-  'Senators\' Spouses': `SELECT ?res WHERE  {
+  'Senators 2': `SELECT ?res WHERE  {
   ?src ?rel <http://dbpedia.org/class/yago/Senator110578471> .
   ?src <http://dbpedia.org/property/spouse> ?res .
 }`,
-  'Senators who were also Presidents': `SELECT ?src WHERE  {
+  'Senators 1': `SELECT ?src WHERE  {
   ?src ?rel <http://dbpedia.org/class/yago/Senator110578471> .
   ?src ?rel <http://dbpedia.org/class/yago/WikicatPresidentsOfTheUnitedStates> .
 }`,
